d6281f0e6c1aa74d477653e12063426ab6e1df99
unknown <jimpidas>
2021-06-10 19:49:35 +0530

    [Jyosmita]Added: retrieving data with new table structure

--- UseCase12.sql
+++ UseCase12.sql
@@ -7,3 +7,8 @@ select max(p.basicpay) as "maxSalary",e.gender from payroll p inner join Employe
 select min(p.basicpay) as "minSalary",e.gender from payroll p inner join Employee e on p.emp_id=e.emp_id  group by gender ;
 select avg(p.basicpay) as "averageSalry",e.gender from payroll p inner join Employee e on p.emp_id=e.emp_id  group by gender ;
 select count(p.basicpay) as "count",e.gender from payroll p inner join Employee e on p.emp_id=e.emp_id  group by gender ;
+
+
+
+SELECT e.name,e.startdate,e.gender,e.phone,e.address,c.company_name,s.basicpay FROM Employee as e  inner join Company AS c  ON e.emp_id=c.emp_id
+inner join Payroll AS s on e.emp_id=s.emp_id inner join  Department as d on d.dept_id=e.emp_id inner join Employee_Department as ed on ed.dept_id=d.dept_id
